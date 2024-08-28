e7d5b5d719c65f4bf1387bc306d96b078ef40429
elifdemir <ed.demirelif@gmail.com>
2024-08-28 16:49:47 +0200

    code clean up

--- src/util/PrivateSetIntersection.java
+++ src/util/PrivateSetIntersection.java
@@ -170,22 +170,3 @@ public class PrivateSetIntersection {
         return counter;
     }
 }
-
-// Utility class to hold pairs of values
-class Pair<K, V> {
-    private final K first;
-    private final V second;
-
-    public Pair(K first, V second) {
-        this.first = first;
-        this.second = second;
-    }
-
-    public K getFirst() {
-        return first;
-    }
-
-    public V getSecond() {
-        return second;
-    }
-}
